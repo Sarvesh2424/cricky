6291e4b0b00c211346cc6875bfed93179a040373
Sarvesh <10650sarvesh@gmail.com>
2026-04-04 17:23:58 +0530

    login and register page mobile responsive

--- app/register/page.js
+++ app/register/page.js
@@ -5,16 +5,16 @@ function Register() {
   return (
     <>
       <Toaster position="bottom-right" />
-      <div className="min-h-screen flex bg-violet-900">
-        <div className="w-1/2 min-h-screen flex items-center justify-center">
-          <div className="flex flex-col items-center gap-4 mb-24">
+      <div className="flex flex-col md:flex-row md:min-h-screen h-screen bg-violet-900">
+        <div className="min-h-1/3 md:w-1/2 md:min-h-screen flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-4">
             <h1 className="text-white text-6xl">Cricky!</h1>
-            <p className="text-lg text-gray-200">
+            <p className="text-lg text-gray-200 p-2 text-center">
               Track your favourite team and share your CRICKY thoughts!
             </p>
           </div>
         </div>
-        <div className="bg-white flex flex-col items-center justify-center w-1/2 min-h-screen">
+        <div className="bg-white flex flex-col items-center justify-center md:w-1/2 md:min-h-screen h-2/3">
           <RegisterForm />
         </div>
       </div>
