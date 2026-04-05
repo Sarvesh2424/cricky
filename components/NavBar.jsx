0fd7e7e2146aa515b6e6739461c19f6e44bf16f4
Sarvesh <10650sarvesh@gmail.com>
2026-04-05 11:16:35 +0530

    make navbar mobile responsive

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { authClient } from "@/lib/authClient";
-import { LogIn, LogOut } from "lucide-react";
+import { LogIn, LogOut, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
@@ -11,6 +11,7 @@ function NavBar() {
   const pathName = usePathname();
   const router = useRouter();
   const [loggingOut, setIsLoggingOut] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogout = async () => {
     setIsLoggingOut(true);
@@ -27,46 +28,96 @@ function NavBar() {
   };
 
   return (
-    <div className="bg-violet-900 max-w-screen p-4 flex items-center justify-between shadow-2xl">
-      <div className="flex items-center gap-12">
-        <h1 className="text-white text-5xl">Cricky!</h1>
-        <div className="flex items-center gap-4">
-          <Link href="/">
-            <button className="text-white text-lg hover:cursor-pointer">
-              Home
-            </button>
-            {pathName == "/" && <hr className="text-white w-full" />}
-          </Link>
-          <Link href={"/feed"}>
-            <button className="text-white text-lg hover:cursor-pointer">
-              Feed
-            </button>
-            {pathName == "/feed" && <hr className="text-white w-full hx `" />}
-          </Link>
-        </div>
-      </div>
-      {session ? (
-        <button
-          disabled={loggingOut}
-          className={`text-lg text-red-500 ${loggingOut && "hover:cursor-wait"} flex gap-2 items-center hover:cursor-pointer hover:bg-red-300 transition-colors p-2 bg-red-200 rounded-lg`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleLogout();
-          }}
-        >
-          <LogOut />
-          Log Out
-        </button>
-      ) : (
-        <Link href={"/login"}>
+    <div className="bg-violet-900 w-full p-4 flex items-center shadow-2xl">
+      <div className="flex items-center justify-between w-full relative">
+        <div className="flex justify-between md:justify-normal md:gap-12 w-full md:w-auto">
+          <h1 className="text-white text-5xl">Cricky!</h1>
           <button
-            className={`text-lg  flex gap-2 items-center hover:cursor-pointer hover:bg-gray-100 transition-colors p-2 bg-white rounded-lg`}
+            onClick={(e) => {
+              e.preventDefault();
+              setMenuOpen((prev) => !prev);
+            }}
+            className="text-white md:hidden"
           >
-            <LogIn />
-            Log In
+            {menuOpen ? <X /> : <Menu />}
           </button>
-        </Link>
-      )}
+          <div className="hidden md:flex items-center gap-4">
+            <Link href="/">
+              <button className="text-white text-lg hover:cursor-pointer">
+                Home
+              </button>
+              {pathName == "/" && <hr className="text-white w-full" />}
+            </Link>
+            <Link href={"/feed"}>
+              <button className="text-white text-lg hover:cursor-pointer">
+                Feed
+              </button>
+              {pathName == "/feed" && <hr className="text-white w-full hx `" />}
+            </Link>
+          </div>
+        </div>
+
+        {session ? (
+          <button
+            disabled={loggingOut}
+            className={`hidden text-lg text-red-500 ${loggingOut && "hover:cursor-wait"} md:flex  gap-2 items-center hover:cursor-pointer hover:bg-red-300 transition-colors p-2 bg-red-200 rounded-lg`}
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogout();
+            }}
+          >
+            <LogOut />
+            {"Log Out"}
+          </button>
+        ) : (
+          <Link href={"/login"}>
+            <button
+              className={` hidden text-lg h-min md:flex gap-2 items-center hover:cursor-pointer hover:bg-gray-100 transition-colors p-2 bg-white rounded-lg`}
+            >
+              <LogIn />
+              Log In
+            </button>
+          </Link>
+        )}
+        {menuOpen && (
+          <div className="absolute right-0 top-10 bg-violet-950 p-2 rounded-lg flex flex-col">
+            <Link href="/">
+              <button className="text-white text-lg hover:cursor-pointer">
+                Home
+              </button>
+              {pathName == "/" && <hr className="text-white w-full" />}
+            </Link>
+            <Link href={"/feed"}>
+              <button className="text-white text-lg hover:cursor-pointer">
+                Feed
+              </button>
+              {pathName == "/feed" && <hr className="text-white w-full" />}
+            </Link>
+            {session ? (
+              <button
+                disabled={loggingOut}
+                className={`text-lg  text-red-500 mt-2 ${loggingOut && "hover:cursor-wait"} flex gap-2 items-center hover:cursor-pointer hover:bg-red-300 transition-colors p-2 bg-red-200 rounded-lg`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleLogout();
+                }}
+              >
+                <LogOut />
+                Log Out
+              </button>
+            ) : (
+              <Link href={"/login"}>
+                <button
+                  className={`text-lg mt-2 flex gap-2 items-center hover:cursor-pointer hover:bg-gray-100 transition-colors p-2 bg-white rounded-lg`}
+                >
+                  <LogIn />
+                  Log In
+                </button>
+              </Link>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
